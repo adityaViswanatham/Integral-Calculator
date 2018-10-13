6e1839e81a73299fd2d437b3a4b5d3e6af2ec033
av-2599 <43505807+av-2599@users.noreply.github.com>
2018-10-12 19:11:30 -0500

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,17 +1,18 @@
-//Name: Aditya Viswanatham
-//Class: CS 2336.003
-//Project 4
+/** @author: Aditya Viswanatham
+*/
 
 package Paradox;
 
 //Node Class.
 public class Node <E> {
+	
 	//Class Attributes.
 	public E gen_var;
 	//Left Pointer.
 	public Node <E> left;
 	//Right Pointer.
 	public Node <E> right;
+	
 	//Class Constructor.
 	public Node() {};
 	//Overloaded Constructor.
@@ -21,27 +22,25 @@ public class Node <E> {
 		left = null;
 		right = null;
 	}
+	
 	//Setters.
-	public void setLeft(Node <E> left)
-	{
+	public void setLeft(Node <E> left) {
 		this.left = left;
 	}
-	public void setRight(Node <E> right)
-	{
+	public void setRight(Node <E> right) {
 		this.right = right;
 	}
+	
 	//Getters.
-	public Node <E> getLeft()
-	{
+	public Node <E> getLeft() {
 		return left;
 	}
-	public Node <E> getRight()
-	{
+	public Node <E> getRight() {
 		return right;
 	}
+	
 	//GetObject() returns the payload object.
-	public E getObject()
-	{
+	public E getObject() {
 		return gen_var;
 	}
 }
